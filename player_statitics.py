ce8574aa4815f867dbf1d01352b62cbc71225c5a
Bas <30810@ma-web.nl>
2020-09-25 15:26:37 +0200

    derde commit

---
+++ player_statitics.py
@@ -0,0 +1,13 @@
+#player statistics
+print("\n")
+
+name = "Name: Boop the cute one.\n"
+Quirk = "Quirk: give evreyone happines or a cute overdose (user can choose).\n"
+speed = "Speed: 35 k/m\n"
+level1 = "level: "
+level2 = 69
+wanetd1 = "Wanetd: "
+wanetd2 = False
+weapon = "Weapon: Quirk and body"
+
+print(name + Quirk + speed + level1 + str(level2) + "\n"+ wanetd1 +str(wanetd2) + "\n" + weapon)

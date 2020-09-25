22b3a0e73ff1d5f2744b1aa2cff051071f386b35
Bas <30810@ma-web.nl>
2020-09-25 14:40:55 +0200

    tweede commit

---
+++ poem.py
@@ -0,0 +1,9 @@
+#poem
+vers1 = "Hallo ik ben Bas.\n"
+vers2 = "Ik woon in het mooie Zaandam.\n"
+vers3 = "Games, dat speel ik graag, zo als R6, minecraft, among us, ark en city builders.\n"
+vers4 = "Wat ik nog meer doe is anime kijken, weeb mode actevadet.\n"
+vers5 = "Ik ga leren om te programeren.\n"
+vers6 = "Dat was het."
+
+print(vers1, vers2, vers3, vers4, vers5, vers6)

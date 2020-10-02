3a4ae3753f7db4df7983dc455f2ab39cfd9c0218
Bas <30810@ma-web.nl>
2020-10-02 14:24:12 +0200

    heb het verbeterd.

--- My_First_Operations.py
+++ My_First_Operations.py
@@ -10,13 +10,16 @@ print("\nWat is 2 - 1?")
 min_som = 2 - 1
 print(min_som)
 
+print("\nWat is Z x Z")
+print(Z * Z)
+
 print("\nWat is 9 gedeelt door 3?")
 delen_som = 9 / 3
 print(delen_som)
 
-print("\nHelp ik begrijp niet hoe % werkt op python.")
-procent_som = 100 % 4
-print(procent_som)
+print("\nWat is 5 % 10?")
+F = 10
+print(Z % F)
 
 print("\nWat is 2 + 2?")
 x = 2
@@ -27,3 +30,11 @@ print("\nWat is 12 gedeelt door 3?")
 y = 12
 y /= 3
 print(y)
+
+print("\nWat is 2(kleine 3)?")
+I = 2
+print(I ** delen_som)
+
+print("\nWat is het dichtbijste getal als je 15 deelt door 5?")
+H = 15
+print(H // x)

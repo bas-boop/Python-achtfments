284a75295974ed38b3eb44be0f571e756e0d80d9
Bas <30810@ma-web.nl>
2020-10-02 14:04:47 +0200

    My First Operations toegevoegd.

---
+++ My_First_Operations.py
@@ -0,0 +1,29 @@
+print("Wat is Z?")
+Z = 5
+print(Z)
+
+print("\nWat is 1 + 1?")
+plus_som = 1 + 1
+print(plus_som)
+
+print("\nWat is 2 - 1?")
+min_som = 2 - 1
+print(min_som)
+
+print("\nWat is 9 gedeelt door 3?")
+delen_som = 9 / 3
+print(delen_som)
+
+print("\nHelp ik begrijp niet hoe % werkt op python.")
+procent_som = 100 % 4
+print(procent_som)
+
+print("\nWat is 2 + 2?")
+x = 2
+x += 2
+print(x)
+
+print("\nWat is 12 gedeelt door 3?")
+y = 12
+y /= 3
+print(y)

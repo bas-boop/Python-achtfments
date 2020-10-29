395b6fc24d2c188b72b0104fe9583032b78ecb35
Bas <30810@ma-web.nl>
2020-10-29 11:38:02 +0100

    Heb de countdown in 8 min gemaakt :D

---
+++ The_final_countdown.py
@@ -0,0 +1,6 @@
+#rickroll
+for x in range(1001):
+  print(x)
+else:
+  print("\nhttps://www.youtube.com/watch?v=dQw4w9WgXcQ")
+  print("Dit is een video speciaal voor jou, omdat je de countdown heb over leeft.\n")
